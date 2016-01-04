3e6fb1c80b7408666c9be42f31777d3112bc8881
Bruno <bportaluri@gmail.com>
2016-01-04 21:16:29 +0100

    Copyright added

--- src/WiFiEspUdp.cpp
+++ src/WiFiEspUdp.cpp
@@ -1,14 +1,20 @@
+/*--------------------------------------------------------------------
+This file is part of the Arduino WiFiEsp library.
 
-/*
-#include <string.h>
-#include "utility/server_drv.h"
-#include "utility/wifi_drv.h"
+The Arduino WiFiEsp library is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-#include "WiFi.h"
-#include "WiFiUdp.h"
-#include "WiFiClient.h"
-#include "WiFiServer.h"
-*/
+The Arduino WiFiEsp library is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with The Arduino WiFiEsp library.  If not, see
+<http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------*/
 
 #include "WiFiEsp.h"
 #include "WiFiEspUdp.h"
--- src/utility/RingBuffer.h
+++ src/utility/RingBuffer.h
@@ -1,3 +1,21 @@
+/*--------------------------------------------------------------------
+This file is part of the Arduino WiFiEsp library.
+
+The Arduino WiFiEsp library is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+The Arduino WiFiEsp library is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with The Arduino WiFiEsp library.  If not, see
+<http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------*/
+
 #ifndef RingBuffer_h
 #define RingBuffer_h
 
