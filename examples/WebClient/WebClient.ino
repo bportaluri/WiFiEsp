174e505fbd65d59b89984f817aa93a7cabf0f731
Rudi Strydom <drpain@users.noreply.github.com>
2018-02-02 21:17:00 +0200

    Revert

--- examples/WebClient/WebClient.ino
+++ examples/WebClient/WebClient.ino
@@ -7,11 +7,18 @@
  For more details see: http://yaab-arduino.blogspot.com/p/wifiesp-example-client.html
 */
 
-#include <WiFiEsp.h>
+#include "WiFiEsp.h"
+
+// Emulate Serial1 on pins 6/7 if not present
+#ifndef HAVE_HWSERIAL1
+#include "SoftwareSerial.h"
+SoftwareSerial Serial1(6, 7); // RX, TX
+#endif
 
 char ssid[] = "Twim";            // your network SSID (name)
 char pass[] = "12345678";        // your network password
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
 char server[] = "arduino.cc";
 
 // Initialize the Ethernet client object
@@ -22,10 +29,17 @@ void setup()
   // initialize serial for debugging
   Serial.begin(115200);
   // initialize serial for ESP module
-  Serial1.begin(115200);
+  Serial1.begin(9600);
   // initialize ESP module
   WiFi.init(&Serial1);
-  
+
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+
   // attempt to connect to WiFi network
   while ( status != WL_CONNECTED) {
     Serial.print("Attempting to connect to WPA SSID: ");
@@ -75,7 +89,6 @@ void loop()
 
   delay(1);
 }
-
 
 void printWifiStatus()
 {
